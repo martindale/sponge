8ad084453efd16353e8058078c87f2a832bf6042
Erik Hollensbe <erik+github@hollensbe.org>
2013-05-12 19:54:47 -0700

    documentation: some edits to overview

--- sponge.go
+++ sponge.go
@@ -2,14 +2,15 @@
 Sponge is a caching, state-aware proxy library.
 
 Sponge sits between clients and API services and acts very much like
-If-Modified-Since, but doesn't waste time on headers, because most API clients
-don't.
+If-Modified-Since, but doesn't waste time on cache headers, because most API
+clients don't.
 
-It just repeats the result from a cached API request, then starts a background
-poll which hits it over a configurable amount of time, on a configurable
-frequency, and waits for a state change. If a state change occurs, backend
-polling stops, the cache is updated with the new result, and the rest of the
-time is spent just serving from cache.
+It just repeats and caches the result from an API request, then starts a
+background poll which hits it over a configurable amount of time, on a
+configurable frequency, and waits for a state change. If a state change occurs,
+backend polling stops, the cache is updated with the new result, and the rest
+of the time is spent just serving from cache. After cache expiration, the
+process would repeat itself for a given API request.
 
 Again, this is a library -- you are expected to implement your own services
 code, as well as code to generate other functions of the proxy. There is an
